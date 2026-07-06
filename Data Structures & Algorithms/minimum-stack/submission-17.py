e5c6f1a13d1dd0571cbd62ec47ff0b85b8295bba
belalrayan <belalrayan111@gmail.com>
2026-07-07 00:41:39 +0300

    Add: minimum-stack - submission-17

---
+++ Data Structures & Algorithms/minimum-stack/submission-17.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.mins=[]
+        self.elems=[]
+        
+
+    def push(self, val: int) -> None:
+        self.elems.append(val)
+        if len(self.mins) >0:
+            self.mins.append(min(self.mins[-1],val))
+        else:
+            self.mins.append(val)
+            
+
+        
+
+    def pop(self) -> None:
+        if not self.mins:
+            return
+        self.elems.pop()
+        self.mins.pop()
+       
+        
+
+        
+
+    def top(self) -> int:
+        return self.elems[-1]
+
+    def getMin(self) -> int:
+        
+        return self.mins[-1]
+        
