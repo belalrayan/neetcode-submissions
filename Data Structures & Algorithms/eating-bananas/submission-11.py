3c406a32f6068001e659f00cc0c403167fc3f2af
belalrayan <belalrayan111@gmail.com>
2026-07-07 05:41:55 +0300

    Add: eating-bananas - submission-11

---
+++ Data Structures & Algorithms/eating-bananas/submission-11.py
@@ -0,0 +1,39 @@
+class Solution:
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        hours_needed=0
+        left=1
+        right=max(piles)
+        mid=-1
+        best=-1
+        while (left<=right):
+            mid=int((left+right)/2)
+            hours_needed=0
+
+
+            for i in range(len(piles)):
+                hours,partial_hours=divmod(piles[i],mid)
+                hours_needed+=hours
+                if partial_hours>0:
+                    hours_needed+=1
+                if hours_needed>h:
+                    left=mid+1
+                    break
+                
+            if hours_needed <=h:
+                best=mid        
+                right=mid-1
+            hourse_needed=0
+
+        return best    
+
+
+
+
+                
+
+
+
+
+
+        
