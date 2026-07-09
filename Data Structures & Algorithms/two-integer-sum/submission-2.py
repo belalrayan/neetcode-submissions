ad7bc10984e18ff9ce201a681a5a7bc5c56d04f1
belalrayan <belalrayan111@gmail.com>
2026-07-09 22:49:53 +0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hist={}
+        for i,num in enumerate(nums):
+            if target-num in hist:
+                return [hist[target-num],i]
+            hist[num]=i
+
+        
