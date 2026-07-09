dae8d14a12ea85c2b7b2899216e5268c1d90227b
belalrayan <belalrayan111@gmail.com>
2026-07-09 09:44:46 +0300

    Add: clone-graph - submission-5

---
+++ Data Structures & Algorithms/clone-graph/submission-5.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        visited={node.val : Node(node.val)}
+        q=collections.deque([node])
+        while q:
+            curr=q.popleft()
+            clone=visited[curr.val]
+            for neighbor in curr.neighbors:
+                if neighbor.val not in visited:
+                    visited[neighbor.val]=Node(neighbor.val)
+                    q.append(neighbor)
+                clone.neighbors.append(visited[neighbor.val])
+        return visited[node.val]
