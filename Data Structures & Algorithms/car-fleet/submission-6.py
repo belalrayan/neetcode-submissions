a23b70154e3c3be8ea3dd48d2945991ba3e5867b
belalrayan <belalrayan111@gmail.com>
2026-07-10 05:08:30 +0300

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs=reversed(sorted(zip(position,speed)))
+
+        fleets=0
+        last_time_to_target=0
+
+        for pos,spd in pairs:
+            time_to_target=(target-pos)/spd
+
+            if time_to_target >last_time_to_target:
+                fleets+=1
+                last_time_to_target=time_to_target
+
+        return fleets
+
+
+        
