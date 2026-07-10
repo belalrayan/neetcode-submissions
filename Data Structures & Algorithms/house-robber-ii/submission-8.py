adf7314b455e85f7c57a9c770be3a0e1bfe57529
belalrayan <belalrayan111@gmail.com>
2026-07-10 19:27:19 +0300

    Add: house-robber-ii - submission-8

---
+++ Data Structures & Algorithms/house-robber-ii/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len (nums)==1:
+            return nums[0]    
+        
+        def rob_linear(profit):
+            if not profit:
+                return 0
+            if len(profit)==1:
+                return profit[0]    
+            prev1,prev2= profit[0],max(profit[0],profit[1])
+            for i in range(2,len(profit)):
+                curr=max(profit[i]+prev1,prev2)
+                prev1,prev2=prev2,curr
+            return prev2    
+        return max(rob_linear(nums[1:]),rob_linear(nums[:-1]))        
+
+        
