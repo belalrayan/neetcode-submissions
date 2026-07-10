8481e5d302129ffba05cc00bf15bf14012c1f13b
belalrayan <belalrayan111@gmail.com>
2026-07-10 16:32:18 +0300

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+
+        def is_palindrome(string, left, right):
+            while left < right:
+                if string[left] != string[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        def backtrack(start):
+            if start == len(s):
+                res.append(part[:])
+                return
+            
+            for end in range(start, len(s)):
+                # אם תת-המחרוזת מ-start ועד end היא פלינדרום
+                if is_palindrome(s, start, end):
+                    part.append(s[start:end+1])
+                    backtrack(end + 1)
+                    part.pop()
+
+        backtrack(0)
+        return res
